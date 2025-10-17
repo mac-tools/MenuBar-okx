443e52f18b5ce4269411ed91b69c3b3ca45ff859
lismin <lixumin1030@gmail.com>
2025-10-17 11:03:04 +0800

    Initial Commit

---
+++ show-info-bar/show_info_barApp.swift
@@ -0,0 +1,17 @@
+//
+//  show_info_barApp.swift
+//  show-info-bar
+//
+//  Created by lixumin on 2025/10/17.
+//
+
+import SwiftUI
+
+@main
+struct show_info_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
